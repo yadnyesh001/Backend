a7b68bcf8679930b604a8963ae2e55b633c26df2
Yadnyesh Badgujar <yadnyeshbadgujar2004@gmail.com>
2024-12-16 17:38:14 +0530

    Delete 02UrlShortner directory

--- 02UrlShortner/connect.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-async function connectToMongoDB(url) {
-  return mongoose.connect(url); // No need for options in v4+
-}
-
-module.exports = { connectToMongoDB };
